5e2d78cbae1fd12e69f6bc83f238abbfab84d9c3
penguyen <peternguyen008@gmail.com>
2016-06-23 16:56:58 -0400

    initial xcode emulator load

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -17,7 +11,7 @@ class Padticular2 extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          WHATTUP PETE!! Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
